515117c7c49a59cadef3281050c9e9312684c54e
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2024-05-05 15:36:07 +0300

    add js

--- js/index.js
+++ js/index.js
@@ -1,57 +1,32 @@
-// function showForm(formNumber) {
-//   const form1 = document.getElementById('form1');
-//   const form2 = document.getElementById('form2');
-
-//   if (formNumber === 1) {
-//       form1.style.display = 'block';
-//       form2.style.display = 'none';
-//   } else if (formNumber === 2) {
-//       form1.style.display = 'none';
-//       form2.style.display = 'block';
-//   }
-// }
-
-// function showForm(formNumber) {
-//   const form1 = document.getElementById("form1");
-//   const form2 = document.getElementById("form2");
-//   const img1 = document.getElementById("activeImg1");
-//   const img2 = document.getElementById("activeImg2");
-
-//   if (formNumber === 1) {
-//     form1.style.display = "block";
-//     form2.style.display = "none";
-//     img1.style.display = "inline"; // Показати іконку для форми 1
-//     img2.style.display = "none"; // Сховати іконку для форми 2
-//   } else if (formNumber === 2) {
-//     form1.style.display = "none";
-//     form2.style.display = "block";
-//     img1.style.display = "none"; // Сховати іконку для форми 1
-//     img2.style.display = "inline"; // Показати іконку для форми 2
-//   }
-// }
-
-
 function showForm(formNumber) {
-  var form1 = document.getElementById('form1');
-  var form2 = document.getElementById('form2');
-  var img1 = document.getElementById('activeImg1');
-  var img2 = document.getElementById('activeImg2');
-  var link1 = document.querySelector('.link1');
-  var link2 = document.querySelector('.link2');
+  const form1 = document.getElementById("form1");
+  const form2 = document.getElementById("form2");
+  const img1 = document.getElementById("activeImg1");
+  const img2 = document.getElementById("activeImg2");
+  const link1 = document.querySelector(".dr_link1");
+  const link2 = document.querySelector(".dr_link2");
 
   if (formNumber === 1) {
-      form1.style.display = 'block';
-      form2.style.display = 'none';
-      img1.style.display = 'inline'; // Показати іконку для форми 1
-      img2.style.display = 'none';   // Сховати іконку для форми 2
-      link1.classList.add('dr_active-link');
-      link2.classList.remove('dr_active-link');
-  } else if (formNumber === 2) {
-      form1.style.display = 'none';
-      form2.style.display = 'block';
-      img1.style.display = 'none';   // Сховати іконку для форми 1
-      img2.style.display = 'inline'; // Показати іконку для форми 2
-      link1.classList.remove('dr_active-link');
-      link2.classList.add('dr_active-link');
+    toggleForms(form1, form2, img1, img2, link1, link2);
+  } else {
+    toggleForms(form2, form1, img2, img1, link2, link1);
   }
+}
+
+function toggleForms(
+  activeForm,
+  inactiveForm,
+  activeImg,
+  inactiveImg,
+  activeLink,
+  inactiveLink
+) {
+  activeForm.style.display = "block";
+  inactiveForm.style.display = "none";
+  activeImg.style.display = "inline";
+  inactiveImg.style.display = "none";
+  activeLink.classList.add("dr_active-link");
+  activeLink.classList.remove("dr_form-link");
+  inactiveLink.classList.remove("dr_active-link");
+  inactiveLink.classList.add("dr_form-link");
 }
